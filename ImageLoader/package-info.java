3217960474d8d337057f387726182a6f1eb863a9
Sukant Kumar <sukant101@users.noreply.github.com>
2014-03-17 11:28:08 +0530

    Create package-info.java

---
+++ ImageLoader/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains all the classes for loading the image from the URL
+ * Displaying it in the image view
+ * Scaling
+ * Memory Caching
+ */
+/**
+ * @author sukant
+ *
+ */
+package com.example.xxxx;
